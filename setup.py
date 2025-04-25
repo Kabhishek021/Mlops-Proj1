797dd00ad3cbd9f24c7776eb14e548a3706b9ceb
abhishek <dskumar021@gmail.com>
2025-04-25 14:26:06 +0530

    project structure ready ,venv set

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup , find_packages
+
+setup(
+
+        name = "src",
+        version = "0.0.1",
+        author = "Kumar Abhishek",
+        author_email = "kumar2025@gmail.com",
+        package = find_packages()
+
+
+
+
+)
